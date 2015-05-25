a88b99c0106b0fe81dbec2f41233db8fff91ad88
Nguyen Ngoc Thanh Hai <hainnt1302@gmail.com>
2015-05-25 10:16:59 +0700

    Init Solution with Web Project and Entity Project

---
+++ code/backend/CP-MathHub/CP-MathHub/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CP_MathHub.Entity;
+
+namespace CP_MathHub.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            User user = new User();
+            ReputationRule repu = new ReputationRule();
+            CPMathHubModelContainer ctx = new CPMathHubModelContainer();
+            repu = ctx.ReputationRules.Find(1);
+            ViewBag.Repu = repu;
+
+            return View();
+        }
+    }
+}
