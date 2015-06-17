26bec22c7580c408a619d7150062e9231d25a231
minhnd93 <minhnd081293@gmail.com>
2015-06-17 12:50:29 +0700

    Discussion UpDate

---
+++ code/backend/CP-MathHub/CP-MathHub/AutoMapper/AutoMapperProfile/DiscussionMapperProfile.cs
@@ -0,0 +1,27 @@
+﻿using Profile = AutoMapper.Profile;
+using CP_MathHub.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CP_MathHub.Models.Discussion;
+using CP_MathHub.DAL;
+using AutoMapper;
+
+namespace CP_MathHub.AutoMapper.AutoMapperProfile
+{
+    public class DiscussionMapperProfile :Profile
+    {
+        /// <summary>
+        /// Config profile for System AutoMapper
+        /// </summary>
+        protected override void Configure()
+        {
+            // Discussion Preview      
+            Mapper.CreateMap<Discussion, DiscussionPreviewViewModel>();
+
+            // Discussion Detail
+            Mapper.CreateMap<Discussion, DiscussionDetailViewModel>();
+        }
+    }
+}
---
+++ code/backend/CP-MathHub/CP-MathHub/Models/Discussion/DiscussionCreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CP_MathHub.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CP_MathHub.Models.Discussion
+{
+    public class DiscussionCreateViewModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength = 10)]
+        public string Title { get; set; }
+        [Required]
+        [MinLength(20)]
+        public string Content { get; set; }
+        public List<String> Tags { get; set; }
+    }
+}
