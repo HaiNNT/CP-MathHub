269fcf8d018ff8aab03c9a684f8f4ee25eaacea9
Nguyen Ngoc Thanh Hai <hainnt1302@gmail.com>
2015-06-18 10:52:56 +0700

    init fontend not finished ; add common widget not finished

---
+++ code/backend/CP-MathHub/CP-MathHub/Content/js/infinity-loading.js
@@ -0,0 +1,69 @@
+﻿/**
+ * Author: HaiNNT
+ * Created Date: 2015-06-18
+ * Description: This script file store 'infinity loading' script for infinity scroll feature.
+ */
+
+var il_page = 0;
+var il_ready = true;
+var IL_MAIN_POST_TYPE_QUESTION = "question";
+var IL_MAIN_POST_TYPE_ARTICLE = "article";
+var IL_MAIN_POST_TYPE_DISCUSSION = "discussion";
+
+$(document).ready(function () {
+    var type = $("#system").val;
+    $(window).scroll(function () {
+        if (($(window).scrollTop() + $(window).height() > $(document).height() - 1500) && il_ready) {
+            il_ready = false;
+            getMoreMainPost(type);
+        }
+    });
+})
+
+/*
+Get more main post from server by ajax
+
+@author 
+@this {function}
+@param {string} type : Type of main post to loading more.
+ */
+function getMoreMainPost(type) {
+    var list; //current list main post {list of jquery entities}
+    var url; //server url {string}
+    var data; //request param {object}
+    switch(type) {
+        case MAIN_POST_TYPE_QUESTION:
+            list = $("#list-questions");
+            url = "Question/Index";
+            data = { tab: "Newest", page: ++il_page }
+            break;
+        //case MAIN_POST_TYPE_DISCUSSION:
+        //    list = $("#list-discussions");
+        //    url = "Discussion/Index";
+        //    data = { tab: "Newest", page: ++il_page }
+        //    break;
+        //case MAIN_POST_TYPE_ARTICLE:
+        //    list = $("#list-articles");
+        //    url = "Blog/Index";
+        //    data = { tab: "Newest", page: ++il_page }
+        //    break;
+        default:
+            list = $("#list-questions");
+            url = "Question/Index";
+            data = { tab: "Newest", page: ++il_page }
+            break;
+    }
+        
+    $.ajax({
+        method: "GET",
+        url: url,
+        data: data
+    })
+      .done(function (msg) {
+          list.append(msg);
+          il_ready = true;
+      })
+      .fail(function () {
+          alert("error");
+      });
+}
---
+++ code/backend/CP-MathHub/CP-MathHub/Controllers/CommonWidgetController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CP_MathHub.Framework.Controllers;
+using CP_MathHub.Core.Interfaces.Services;
+using CP_MathHub.Core.Configuration;
+using CP_MathHub.Service.Services;
+using CP_MathHub.Service.Helpers;
+using CP_MathHub.Models.Question;
+using CP_MathHub.Entity;
+using AutoMapper;
+
+namespace CP_MathHub.Controllers
+{
+    [ChildActionOnly]
+    public partial class CommonWidgetController : BaseController
+    {
+        private IAccountService aService;
+
+        public CommonWidgetController()
+        {
+            aService = new AccountService();
+        }
+
+        #region User Widget
+        //public virtual ActionResult ProfileWidget()
+        //{
+
+        //    User user = _userQueryService.GetLoginUser();
+        //    ProfileWidgetVM profileWidgetVm = Mapper.Map<User, ProfileWidgetVM>(user);
+
+        //    return PartialView("_ProfileWidget", profileWidgetVm);
+        //}
+
+        //public virtual ActionResult FavoriteTagWidget()
+        //{
+        //    ICollection<Tag> tags = _userQueryService.GetLoginFavoriteTag().ToList();
+        //    return PartialView("_FavoriteTagWidget", tags);
+        //}
+
+        //public virtual ActionResult GroupWidget()
+        //{
+        //    return PartialView("_GroupWidget");
+        //}
+
+        //public virtual ActionResult MySubscriptionWidget()
+        //{
+
+        //    return PartialView("_MySubscriptionWidget");
+        //}
+
+        //public virtual ActionResult SubscriptionWidget()
+        //{
+        //    ICollection<Subscription> subscriptions = _userQueryService.GetLoginAllSubscriptions().ToList();
+        //    return PartialView("_SubscriptionWidget", subscriptions);
+        //}
+
+        //public virtual ActionResult YourActivityWidget()
+        //{
+        //    return PartialView("_YourActivityWidget");
+        //}
+        #endregion
+    }
+}
