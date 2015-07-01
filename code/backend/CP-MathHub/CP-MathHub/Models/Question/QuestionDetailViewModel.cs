555799efca639b34da87ad68fdd6695b1364fbd2
Nguyen Ngoc Thanh Hai <hainnt1302@gmail.com>
2015-07-01 18:33:51 +0700

    apply detail article not tested, apply list/grid view not tested

--- code/backend/CP-MathHub/CP-MathHub/Models/Question/QuestionDetailViewModel.cs
+++ code/backend/CP-MathHub/CP-MathHub/Models/Question/QuestionDetailViewModel.cs
@@ -14,7 +14,6 @@ namespace CP_MathHub.Models.Question
         public DateTime CreatedDate { get; set; }
         public DateTime LastEditedDate { get; set; }
         public int View { get; set; }
-        public int HintNum { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
         public int ReportNum { get; set; }
