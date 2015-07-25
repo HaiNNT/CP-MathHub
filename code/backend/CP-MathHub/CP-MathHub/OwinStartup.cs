bbe4216734997e8566ded92fadca2a9e9659704c
Nguyen Ngoc Thanh Hai <hainnt1302@gmail.com>
2015-07-25 22:37:57 +0700

    add realtime chat not finished with demo page

---
+++ code/backend/CP-MathHub/CP-MathHub/OwinStartup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(CP_MathHub.OwinStartup))]
+
+namespace CP_MathHub
+{
+    public class OwinStartup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            
+        }
+    }
+}
